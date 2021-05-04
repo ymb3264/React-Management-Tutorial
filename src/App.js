f681432418e0a9880d03ab7cd4d61a690a5c62fc
ymb3264 <a01028371042@gmail.com>
2021-05-05 00:13:45 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '윤모불',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '960305',
+  'gender': '남자',
+  'job': '프로그래머'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이순신',
+  'birthday': '921205',
+  'gender': '남자',
+  'job': '디자이너'
+}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Customer
+        id={customers[0].id}
+        image={customers[0].image}
+        name={customers[0].name}
+        birthday={customers[0].birthday}
+        gender={customers[0].gender}
+        job={customers[0].job}
+      />
+      <Customer
+        id={customers[1].id}
+        image={customers[1].image}
+        name={customers[1].name}
+        birthday={customers[1].birthday}
+        gender={customers[1].gender}
+        job={customers[1].job}
+      />
+      <Customer
+        id={customers[2].id}
+        image={customers[2].image}
+        name={customers[2].name}
+        birthday={customers[2].birthday}
+        gender={customers[2].gender}
+        job={customers[2].job}
+      />
     </div>
   );
 }
